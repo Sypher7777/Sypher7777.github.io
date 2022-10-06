3f7e5673c3213188c63e0d950e761f3a9a7cef0d
Sypher7777 <dbreezie11@gmail.com>
2022-10-06 01:00:08 +0000

    still working on the cat

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,5 +1,30 @@
 var counter1=0
+var counter2=0
+var counter3=0
+var counter4=0
+var counter5=0
 function likePhoto() {
-    console.log ("like")
+    counter1 += 1
 }
+Object.create(user); {
+
+}
+function likePhoto2() {
+    counter2 += 1
+}
+function likePhoto2() {
+    counter3 += 1
+}
+function likePhoto2() {
+    counter4 += 1
+}
+function likePhoto2() {
+    counter5 += 1
+};
+
+
 document.getElementById("likeCount1").innerHTML = counter1;
+document.getElementById("likeCount1").innerHTML = counter2;
+document.getElementById("likeCount1").innerHTML = counter3;
+document.getElementById("likeCount1").innerHTML = counter4;
+document.getElementById("likeCount1").innerHTML = counter5;
