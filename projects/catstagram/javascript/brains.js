fa4654bcdc939e5dc7891a5a491d789717f58cb5
Sypher7777 <dbreezie11@gmail.com>
2022-10-06 14:16:34 +0000

    cats almost finished, just gotta put the pfp

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,14 +1,16 @@
-var counter1=0
-var counter2=0
-var counter3=0
-var counter4=0
-var counter5=0
+var counter1 = 0
+var counter2 = 0
+var counter3 = 0
+var counter4 = 0
+var counter5 = 0
 function likePhoto() {
     counter1 += 1
 }
-Object.create(user); {
-
-}
+const info = new info1();
+info.name = Masahiro;
+info.name_last = Sakurai;
+info.followers = 705000;
+info.profile_pic = placeholder;
 function likePhoto2() {
     counter2 += 1
 }
@@ -28,3 +30,7 @@ document.getElementById("likeCount1").innerHTML = counter2;
 document.getElementById("likeCount1").innerHTML = counter3;
 document.getElementById("likeCount1").innerHTML = counter4;
 document.getElementById("likeCount1").innerHTML = counter5;
+setTimeout(function () { document.getElementById("name").innerHTML = user.name; }, 3000);
+setTimeout(function () { document.getElementById("name_last").innerHTML = user.name_last; }, 3000);
+setTimeout(function () { document.getElementById("followers").innerHTML = "Followers: " + user.followers.length; }, 3000);
+setTimeout(function () { $("#profile_pic").attr("src", user.profile_pic); }, 3000);
