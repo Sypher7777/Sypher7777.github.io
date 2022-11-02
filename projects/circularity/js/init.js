f0ace58c3e41639f3c0c6cc338f8202db5d9b720
Sypher7777 <dbreezie11@gmail.com>
2022-11-02 23:53:20 +0000

    circles

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle;
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+ physikz.addRandomVelocity(circle, canvas);
+ view.addChild(circle);
+ circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +48,17 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            physikz.updatePosition(circles[0])
+            physikz.updatePosition(circles[1])
+            physikz.updatePosition(circles[2])
+            physikz.updatePosition(circles[3])
+            physikz.updatePosition(circles[4])
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            game.checkCirclePosition(circles[0])
+            game.checkCirclePosition(circles[1])
+            game.checkCirclePosition(circles[2])
+            game.checkCirclePosition(circles[3])
+            game.checkCirclePosition(circles[4])
 
             // TODO 9 : Iterate over the array
            
@@ -60,6 +76,15 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
+            // if (circle.x < canvas.width) {
+            //     circle.x = canvas.width;
+            // }
+            if (circle.y > canvas.length) {
+                circle.y = 0
+            }
+            // if (circle.y < canvas.length) {
+            //     circle.y = canvas.length
+            // }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
