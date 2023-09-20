90f24c5047710bc591ce47282ea12ae5d78d68d3
IoT <iot@Operations-Mac-mini-6.local>
2023-09-19 19:44:25 -0500

    started AYBS

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,22 @@
 // requestServer.js file
+
+const http = require("http");
+const request = require("request");
+
+var port = 6768;
+
+http.createserver(function (req, res) {
+    request("https://Sypher7777.github.io", function (error, response, body) {
+      if (!body || !response || (error === null && response.statusCode !== 200)) {
+        res.end("bad URL\n");
+        return;
+
+        if (response.statusCode === 200 && !error === true){
+            
+        }
+      }
+
+    });
+
+  })
+  .listen(6768);
