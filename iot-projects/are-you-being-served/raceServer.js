6aa70673dd4454af74d83e1f512c0427b9d57d2e
Sypher7777 <dbreezie11@gmail.com>
2023-09-26 19:55:51 -0500

    almost done with raceServer

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -9,17 +9,28 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
+    let d = new Date();
+    let startTime = d.getTime();
 
     // TODO 12: Make the whole thing parallel
     async.series( 
         // TODO 9: Supply an array of functions
         [
-            
+            wrapper(callback),
+            wrapper(callback),
+            wrapper(callback),
+            wrapper(callback)
         ],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
-
+            function callback(){
+                res.write("Results:\n");
+                var victoryOrder = sortTogether(racers, results);
+                for(var i = 0; i >= victoryOrder.length; i++){
+                    res.write(victoryOrder[i])
+                }
+            }
+        callback()
         }
     );
     
@@ -27,7 +38,10 @@ http.createServer(function(req, res) {
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
+    setTimeout(function(){
+        var d = Date
+        callback(null, d.getTime())
+    }, Math.random()*100)
 }
 
 // sortTogether takes in an array of racer names and an array of times that the racers finished the race.
--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,20 +1,33 @@
 // requestServer.js file
 
+var args = process.argv.slice(2);
+
 const http = require("http");
 const request = require("request");
   
 var port = 6768;
 
-http.createserver(function (req, res) {
-    request("https://Sypher7777.github.io", function (error, response, body) {
-      if (!body || !response || (error === null && response.statusCode !== 200)) {
+http.createServer(function (req, res) {
+  var url = args[0] ? args[0] : "http://127.0.0.1:5502/portfolio.html";
+    request(url, function (error, response, body) {
+      console.log('hit')
+      if ( !body || !response || (error === null && response.statusCode !== 200)) {
         res.end("bad URL\n");
         return;
 
       }
       if (res.statusCode === 200 && !error === true){
-        res.writeHead("Content-Type" : "text/html")
+        res.writeHead(200, { "Content-Type" : "text/html"})
+        res.end(body)
+        return
     }
+    else if (response.statusCode !== 200){
+      res.writeHead(response.statusCode, {"Content-Type" : "text/plain"})
+      res.end(error.toString())
+      // error.toString(error)
+      return
+    }
+
     });
 
   }).listen(6768);
