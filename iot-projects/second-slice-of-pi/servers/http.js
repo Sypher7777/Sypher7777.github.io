9af2d3762b1e90f6430cfbf434e36ae8e9abcad6
Sypher7777 <dbreezie@gmail.com>
2023-12-12 19:55:12 -0600

    tuus mater2

--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,9 +1,13 @@
+var middlewareConverter = require('./../middleware/converter')
+var bodyParser = require('body-parser')
+
 var actuatorRoutes = require('./../routes/actuators');
 var sensorRoutes = require('./../routes/sensors');
 const express = require('express'),
 	cors = require('cors');
 	
 var app = express();
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/pi/sensors', sensorRoutes);
 app.use('/pi/actuators', actuatorRoutes);
@@ -15,7 +19,7 @@ app.get('/pi', function(req,res){
 	res.send("base, pi");
 });
 
-
+app.use(converter());
 
 module.exports = app;
 
