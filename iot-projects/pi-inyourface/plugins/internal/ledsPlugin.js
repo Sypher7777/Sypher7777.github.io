0e57b54ed590b3d6dceb037af917dae081ca9e4e
Sypher7777 <dbreezie@gmail.com>
2023-12-12 19:11:26 -0600

    tuus mater

--- iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
+++ iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
@@ -10,3 +10,37 @@ exports.start = function (params) {
 };
 
 // TODO 1: Complete the ledsPlugin!
+
+function connectHardware(){
+	import onoff
+	 sensor.watch(function(err, value){
+		if(err){
+			device.value= !!value
+		}
+	}
+	
+function stop(){
+	actuator1.write(0)
+	actuator2.write(0)
+	unexport(actuator1)
+	unexport(actuator2)
+	}
+	
+exports.switchOnOff = {
+    1: function (value) {
+		if(value === 1){
+			actuator1.write(1)
+			}
+		else if(value === 0){
+			actuator1.write(0)
+			}
+    },
+    2: function (value) {
+        		if(value === 1){
+			actuator2.write(1)
+			}
+		else if(value === 0){
+			actuator2.write(0)
+			}
+    }
+}
