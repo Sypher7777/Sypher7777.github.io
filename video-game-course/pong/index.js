0f039fe0e5f04f56d49de3bdfeea29954d3f681a
Sypher7777 <dbreezie11@gmail.com>
2024-02-09 14:23:30 +0000

    code needs reviewing

--- video-game-course/pong/index.js
+++ video-game-course/pong/index.js
@@ -134,11 +134,11 @@
 
     // TODO 3: bounce the ball off each of the paddles
     if(
-       ((ball.right < paddlePlayer.left)) ||
-      ((ball.left < paddlePlayer.right)) ||
+       ((ball.right < paddlePlayer.left) && (ball.y > paddlePlayer.up || ball.y < paddlePlayer.down)) ||
+      ((ball.left < paddlePlayer.right) && (ball.y > paddlePlayer.up || ball.y < paddlePlayer.down)) ||
       
-       (ball.right > paddleCPU.left) ||
-       (ball.left > paddleCPU.right)
+       (ball.right > paddleCPU.left) && (ball.y > paddleCPU.up || ball.y < paddleCPU.down) ||
+       (ball.left > paddleCPU.right) && (ball.y > paddleCPU.up || ball.y < paddleCPU.down)
     )
     {
       createjs.Sound.play("hit")
