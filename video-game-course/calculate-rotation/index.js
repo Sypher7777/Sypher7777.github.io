9a91b323b75b91e04fa9f97ced321943ff013ca3
Sypher7777 <dbreezie11@gmail.com>
2024-03-20 00:57:47 +0000

    doing stuf

--- video-game-course/calculate-rotation/index.js
+++ video-game-course/calculate-rotation/index.js
@@ -1,31 +1,29 @@
 // TODO 4: add a param for your game lib last //
-(function(window, opspark) {
-  console.log('index.js initialized!');
+(function (window, opspark, S7) {
+  console.log("index.js initialized!");
 
-  const
-    assets = opspark.assets,
+  const assets = opspark.assets,
     engine = opspark.V6().activateResize(),
     canvas = engine.getCanvas(),
     stage = engine.getStage(),
-    textfield = assets.makeTextfield('Degrees: ');
-  
+    textfield = assets.makeTextfield("Degrees: ");
+
   stage.addChild(textfield);
 
   // try a different hex color if you want //
-  const ship = assets.makeShip('#4286f4');
-  
-  
+  const ship = assets.makeShip("#C02DFF");
+
   // TODO 5: Center the ship on the stage //
-  
+  ship.x = canvas.x / 2;
+  ship.y = canvas.y / 2;
 
   // TODO 6: Add the ship to the stage //
-  
+  stage.addChild(ship);
 
-  
   function update(event) {
     /*
-     * TODO 7: Use your game lib's getAngleDegrees to get 
-     * the degrees of the angle between the ship and the 
+     * TODO 7: Use your game lib's getAngleDegrees to get
+     * the degrees of the angle between the ship and the
      * mouse position, and assign it to a const called
      * degrees.
      *
@@ -35,22 +33,16 @@
      * these values such that they're packed into a point?
      */
     
-    
     // TODO 8: Set the ship's rotation property to the degrees //
-    
-    
-    
     /*
-     * TODO 9: Uncomment the line below to update the textfield  
-     * with the current angle degrees. Degrees will be a value 
+     * TODO 9: Uncomment the line below to update the textfield
+     * with the current angle degrees. Degrees will be a value
      * between π and -π, or, 180 and -180.
      */
     // assets.updateText(textfield, `Degrees: ${degrees.toFixed(3)}°`, canvas);
   }
 
-  engine
-    .addTickHandlers(update)
-    .activateTick();
+  engine.addTickHandlers(update).activateTick();
 
-// TODO 3: pass your game lib last with, window.my-game-lib //
-}(window, window.opspark));
+  // TODO 3: pass your game lib last with, window.my-game-lib //
+})(window, window.opspark, window.S7);
